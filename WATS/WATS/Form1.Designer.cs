a467721a2bd953405e7a3eff8bd49b8c4e9ed232
MorreseMorrison <rese298@gmail.com>
2024-04-09 19:22:57 -0400

    Add files via upload

--- WATS/WATS/Form1.Designer.cs
+++ WATS/WATS/Form1.Designer.cs
@@ -36,11 +36,13 @@
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // monthCalendar1
@@ -52,38 +54,38 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 101);
+            this.button1.Location = new System.Drawing.Point(12, 130);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(269, 29);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Assign \\ Unassign Office Assets";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 160);
+            this.button2.Location = new System.Drawing.Point(12, 180);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(269, 29);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Assign \\ Unassign Computer Assets";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 219);
+            this.button3.Location = new System.Drawing.Point(12, 270);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(269, 29);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Create \\ Inactivate Employees";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 279);
+            this.button4.Location = new System.Drawing.Point(12, 315);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(269, 29);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
+            this.button4.Text = "Create \\ Inactivate Offices";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // label1
@@ -103,45 +105,76 @@
             this.dataGridView1.Location = new System.Drawing.Point(337, 101);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(254, 58);
+            this.dataGridView1.Size = new System.Drawing.Size(254, 155);
             this.dataGridView1.TabIndex = 6;
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(337, 190);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(254, 58);
-            this.dataGridView2.TabIndex = 7;
             // 
             // button5
             // 
             this.button5.Location = new System.Drawing.Point(337, 270);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(108, 38);
+            this.button5.Size = new System.Drawing.Size(108, 60);
             this.button5.TabIndex = 8;
-            this.button5.Text = "button5";
+            this.button5.Text = "Test Database Connection";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
             this.button6.Location = new System.Drawing.Point(483, 270);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(108, 38);
+            this.button6.Size = new System.Drawing.Size(108, 60);
             this.button6.TabIndex = 9;
-            this.button6.Text = "button6";
+            this.button6.Text = "Show All (Employees, NBV, Offices)";
             this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 361);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(269, 29);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Create Assets";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(65, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Asset Assignment Screens";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(86, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Creation Screens";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(946, 434);
+            this.ClientSize = new System.Drawing.Size(946, 414);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
@@ -151,10 +184,9 @@
             this.Controls.Add(this.monthCalendar1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "WATS";
+            this.Text = "WATS©";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,9 +201,12 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
